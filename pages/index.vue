074d59f5cc9d389c9b9b5ac0dbae218de278635e
Hayden Beck <haydenbeck22@gmail.com>
2021-11-21 20:34:48 -0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center flex-col">
+  <nuxt-img src="/car.jpg" width="1000"/>
+
+  <nuxt-link class="bg-gray-600 text-white w-24" to="/blog">Learn more</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
